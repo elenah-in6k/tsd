e7bf509f24336f9c3cdcae866b661df829248205
elenah-in6k <elenah@interlink-ua.com>
2016-01-25 01:31:32 +0200

    change smth new

--- js/app.js
+++ js/app.js
@@ -1,10 +1,11 @@
 angular.module('tsd', [])
 .directive('requestButton', function($scope) {
-	$scope.btnIcon = "=";
-	$scope.btname = "=";
-	
   return {
   	restrict: 'E',
+  	scope: {
+                btnicon: '=',
+                btnname: '='
+            },
     template: '<span class="{{btnIcon}} "></span>   <span class="request-button-name">{{btnname}</span>'
   };
 });
