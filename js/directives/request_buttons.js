71ba9e35a7b2a9e48936763881a849d759eacc65
elenah-in6k <elenah@interlink-ua.com>
2016-02-04 09:14:47 +0200

    save all

---
+++ js/directives/request_buttons.js
@@ -0,0 +1,15 @@
+angular.module('tsd').directive('requestButtons', function() {
+  return {
+    restrict: 'E',
+    templateUrl: 'view/request_buttons.html',
+    scope: {
+      options: '=',
+      selected: '='
+    },
+    controller: function($scope) {
+      $scope.select = function (option) {
+        $scope.selected = option
+      }
+    }
+  };
+});
---
+++ js/directives/request_form.js
@@ -0,0 +1,20 @@
+angular.module('tsd').directive('requestForm', function() {
+  return {
+    restrict: 'E',
+    templateUrl: 'view/request_form.html',
+    scope: {
+      selectedButton: '=',
+      form: '=',
+      save: '='
+    },
+    controller: function($scope) {
+      $scope.template = {};
+      $scope.initTemplateName = function() {
+        $scope.template.name = "view/forms/" + $scope.selectedButton.type + ".html"
+      };
+      $scope.$watch('selectedButton', function () {
+        $scope.initTemplateName();
+      })
+    }
+  };
+});
