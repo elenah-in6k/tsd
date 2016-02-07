2d1b1e2f025a5198f6c55f2c7ce2c5a05a7f24bc
ElenaH <elenah@interlink-ua.com>
2016-02-07 03:37:21 +0200

    format code

--- js/controllers/tsd.js
+++ js/controllers/tsd.js
@@ -1,18 +1,17 @@
-angular.module('tsd').controller('TsdController', function($scope, $cookies) {
-  $scope.buttons = [
-    { name: 'Запрос на замену ремонт оборудования', class: "fa-gears", type: "fix_hardware" },
-    { name: 'Запрос на доступ к ресурсам', class: "fa-key", type: "access_resources" },
-    { name: 'Сообщить о дефекте', class: "fa-bug", type: "bug" }
-  ];
-  $scope.forms = {};
-  $scope.buttons.forEach(function (button) {
-    $scope.forms[button.type] = $cookies.getObject(button.type)
-  });
-  console.log($scope.forms);
+angular.module('tsd').controller('TsdController', function ($scope, $cookies) {
+    $scope.buttons = [
+        {name: 'Запрос на замену ремонт оборудования', class: "fa-gears", type: "fix_hardware"},
+        {name: 'Запрос на доступ к ресурсам', class: "fa-key", type: "access_resources"},
+        {name: 'Сообщить о дефекте', class: "fa-bug", type: "bug"}
+    ];
+    $scope.forms = {};
+    $scope.buttons.forEach(function (button) {
+        $scope.forms[button.type] = $cookies.getObject(button.type)
+    });
+    console.log($scope.forms);
 
-  $scope.save = function (form) {
-    $cookies.putObject($scope.selectedButton.type, form);
-    console.log(form);
-  }
-
+    $scope.save = function (form) {
+        $cookies.putObject($scope.selectedButton.type, form);
+        console.log(form);
+    }
 });
--- js/directives/request_buttons.js
+++ js/directives/request_buttons.js
@@ -1,15 +1,15 @@
-angular.module('tsd').directive('requestButtons', function() {
-  return {
-    restrict: 'E',
-    templateUrl: 'view/request_buttons.html',
-    scope: {
-      options: '=',
-      selected: '='
-    },
-    controller: function($scope) {
-      $scope.select = function (option) {
-        $scope.selected = option
-      }
-    }
-  };
+angular.module('tsd').directive('requestButtons', function () {
+    return {
+        restrict: 'E',
+        templateUrl: 'view/request_buttons.html',
+        scope: {
+            options: '=',
+            selected: '='
+        },
+        controller: function ($scope) {
+            $scope.select = function (option) {
+                $scope.selected = option
+            }
+        }
+    };
 });
