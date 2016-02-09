ae9fd20aa6fb299c9c6066547dbff68e5fdb0124
elenah-in6k <elenah@interlink-ua.com>
2016-02-09 19:34:29 +0200

    add dropdown resources, change ng-repeat

--- js/controllers/tsd.js
+++ js/controllers/tsd.js
@@ -1,9 +1,10 @@
-angular.module('tsd').controller('TsdController', function ($scope, $cookies) {
+angular.module('tsd').controller('TsdController', function ($scope, $rootScope, $cookies) {
     $scope.buttons = [
         {name: 'Запрос на замену ремонт оборудования', class: "fa-gears", type: "fix_hardware"},
         {name: 'Запрос на доступ к ресурсам', class: "fa-key", type: "access_resources"},
         {name: 'Сообщить о дефекте', class: "fa-bug", type: "bug"}
     ];
+
     $scope.forms = {};
     $scope.buttons.forEach(function (button) {
         $scope.forms[button.type] = $cookies.getObject(button.type)
--- js/directives/request_form.js
+++ js/directives/request_form.js
@@ -8,6 +8,12 @@ angular.module('tsd').directive('requestForm', function () {
             save: '='
         },
         controller: function ($scope) {
+            $scope.fixHardware = {
+                reasons: ['Breakdown', 'Upgrade', 'Individual preference', 'Other'],
+                criticalityLevels: ['Low: nice-to-have', 'Medium: ready to be patient, but not for too long', 'High: blocker for work']
+            };
+            $scope.services = ['Tahometer Ltd', 'HR Grades', 'HR Portal'];
+
             $scope.template = {};
             $scope.initTemplateName = function () {
                 $scope.template.name = "view/forms/" + $scope.selectedButton.type + ".html"
